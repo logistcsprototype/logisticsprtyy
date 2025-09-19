36b4f3577470a46006b2eb505fb2be837c02537c
estifwonde1 <estifanoswondwossen396@gmail.com>
2025-09-19 09:19:00 +0300

    working on the vehicle id

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,7 +5,7 @@ class Admin < ApplicationRecord
   has_many :drivers
   has_many :driver_assignments
   has_many :maintenance_records, class_name: 'Maintenance'
-  has_many :driver_performances_reports
+  has_many :driver_performance_reports
   has_many :insurance_documents
 
   validates :email, presence: true, uniqueness: true
